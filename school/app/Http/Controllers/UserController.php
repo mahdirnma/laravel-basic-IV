3393701f9cb066ce332c9a2457ab0ec237a906fc
mahdirnma <rahnamamahdi78@gmail.com>
2024-08-08 11:08:02 +0330

    create login and signin panel and also build login and signin process

--- school/app/Http/Controllers/UserController.php
+++ school/app/Http/Controllers/UserController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        if (!session()->has('user')) {
+            return view('login');
+        }
+        return view('admin.dashboard');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+    public function signin()
+    {
+        return view('signin');
+    }
 }
