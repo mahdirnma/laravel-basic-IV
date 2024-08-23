c54c2044a1f40682059a81b762bf71429be41de8
mahdirnma <rahnamamahdi78@gmail.com>
2024-08-23 16:34:35 +0330

    create dashboard panel

--- huge_shop/app/Http/Controllers/UserController.php
+++ huge_shop/app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $products = Product::all()->where('is_active',1)->count();
+        $categories = Category::all()->where('is_active',1)->count();
+        return view('admin.index',compact('products','categories'));
+    }
 }
--- huge_shop/resources/views/admin/index.blade.php
+++ huge_shop/resources/views/admin/index.blade.php
@@ -6,11 +6,11 @@
     <div class="w-full h-[88%] bg-gray-200 flex flex-row-reverse p-10">
         <div class="w-1/5 h-48 flex flex-col justify-center items-center ml-12 bg-white rounded-xl">
             <h2 class="mb-8">تعداد محصولات</h2>
-            <p class="text-5xl font-light">{{--{{$hospital}}--}}</p>
+            <p class="text-5xl font-light">{{$products}}</p>
         </div>
         <div class="w-1/5 h-48 flex flex-col justify-center items-center bg-white rounded-xl">
             <h2 class="mb-8">تعداد دسته بندی</h2>
-            <p class="text-5xl font-light">{{--{{$patient}}--}}</p>
+            <p class="text-5xl font-light">{{$categories}}</p>
         </div>
     </div>
 @endsection
